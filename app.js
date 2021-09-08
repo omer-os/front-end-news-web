3f71062ba0a0387ef5edb44fb712c77187e2efb4
omar chatin <omerchatin19@gmail.com>
2021-09-08 21:04:42 +0300

    s

---
+++ app.js
@@ -0,0 +1,7 @@
+$('.burger-btn').click(()=>{
+    $('.nav-mid-l').toggleClass('show');
+
+    $('.line1').toggleClass('animate-l1')
+    $('.line2').toggleClass('animate-l2')
+    $('.line3').toggleClass('animate-l3')
+})
