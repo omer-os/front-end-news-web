be18cb55d2105c1903dae3b8bd788a97805fbf48
omar chatin <omerchatin19@gmail.com>
2021-09-11 01:34:25 +0300

    newDesgin first page completed

---
+++ app.js
@@ -0,0 +1,6 @@
+$('.burgerBtn').click(()=>{
+    $('nav').toggleClass('showRightNav')
+    $('.line-1').toggleClass('line1-animate')
+    $('.line-2').toggleClass('line2-animate')
+    $('.line-3').toggleClass('line3-animate')
+})
