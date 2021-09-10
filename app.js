432a5634f96f63ffcd682889e35e6dd915f44687
omar chatin <omerchatin19@gmail.com>
2021-09-10 22:35:32 +0300

    newdesgin branch first commit

--- app.js
+++
@@ -1,7 +0,0 @@
-$('.burger-btn').click(()=>{
-    $('.nav-mid-l').toggleClass('show');
-
-    $('.line1').toggleClass('animate-l1')
-    $('.line2').toggleClass('animate-l2')
-    $('.line3').toggleClass('animate-l3')
-})
