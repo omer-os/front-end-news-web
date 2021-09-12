e6526957be9ba266a6611e2c560f1a067ad82cee
omar chatin <omerchatin19@gmail.com>
2021-09-12 14:28:53 +0300

    l

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
+const someNews = {
+    
+}
+
+
 $('.burgerBtn').click(()=>{
     $('nav').toggleClass('showRightNav')
     $('.line-1').toggleClass('line1-animate')
     $('.line-2').toggleClass('line2-animate')
     $('.line-3').toggleClass('line3-animate')
 })
+
+
+$(window).scroll(function (event) {
+    var scroll = $(window).scrollTop();
+    if(scroll>=41){
+        $('nav').css({"background-color": "#EDEDED"})
+    }
+    else{
+        $('nav').css({"background-color": "white"})
+    }
+});
